c69ce17af3d9379552713d462a37f224914c9977
SkiHatDuckie <SkiHatDuckie@gmail.com>
2025-08-16 19:41:02 -0400

    Customize graph + Add legend

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,6 @@
 """src/analysis.py"""
 
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 # import numpy as np
 import pandas as pd
@@ -20,20 +21,19 @@ def describe_anxiety_trends(df: pd.DataFrame):
             subgroup_data = df.loc[df["Subgroup"] == subgroup, "Value":]
             util.print_with_indentation(subgroup_data.describe(), num_tabs=2)
 
-def graph_anxiety_trends(df: pd.DataFrame, group: str):
+def graph_anxiety_trends(df: pd.DataFrame, group: str) -> Figure:
     """Graphs the percentage of individuals who reported having anxiety within `group`
     over time. If a group contains multiple subgroups, then each subgroup will be given
     its own line."""
-    _ = plt.subplots(figsize=(16, 5))
     subgroups = df["Subgroup"].loc[df["Group"] == group].unique()
+    fig, ax = plt.subplots(figsize=(14, 5))
     for subgroup in subgroups:
         subgroup_df = df.loc[df["Subgroup"] == subgroup]
         subgroup_df = subgroup_df.set_index("Time Period Start Date")
-        plt.plot(subgroup_df.index, subgroup_df["Value"])
+        ax.plot(subgroup_df.index, subgroup_df["Value"])
 
+    ax.legend(subgroups)
     plt.title(group, fontsize=20)
     plt.xlabel("Time Period Start Date", fontsize=15)
     plt.ylabel("Value", fontsize=15)
-    ticks = list(subgroup_df.index)
-    plt.xticks(ticks, rotation=45)
-    plt.show()
+    return fig
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import inspect
 import json
 import os
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 import analysis
@@ -138,6 +139,7 @@ def run_data_analysis():
             [read_csv_with_dtypes(data_path) for data_path in CLEAN_DATA_PATHS]
         analysis.describe_anxiety_trends(anxiety_df)
         analysis.graph_anxiety_trends(anxiety_df, "By Age")
+        plt.show()
     except FileNotFoundError as ex:
         raise FileNotFoundError("Data must be cleaned first!") \
             from ex.with_traceback(None)
